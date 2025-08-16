69976a82967f5f31a29f10434350abdffd2adcd2
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-08-16 18:39:40 +0530

    s

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return (
-      <main style={{ padding: 32 }}>
+      <main style={{ padding: 32,  height: 500, }}>
         <h1>Page not found</h1>
         <p>The page you’re looking for doesn’t exist or was moved.</p>
       </main>
